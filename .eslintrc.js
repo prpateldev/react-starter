63a996d0a866b1277eb717e9078f7efc0df1683c
Piyush Patel <prpateldev@gmail.com>
2018-08-12 15:04:57 +0530

    eslint and prittier configs update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,36 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "plugins": [
-        "react",
-        "prettier"
+  parser: 'babel-eslint',
+  plugins: ['react', 'prettier'],
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    // "airbnb-base"
+    'airbnb',
+    'prettier',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    quotes: ['error', 'single'],
+    'comma-dangle': ['error', 'never'],
+    indent: ['error', 2],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['to']
+      }
     ],
-    "env": {
-        "browser": true,
-        "node": true
-    },
-    "extends": [ 
-        // "airbnb-base" 
-        "airbnb",
-        "prettier",
-        "plugin:prettier/recommended"
-    ],
-    "rules":{
-        "comma-dangle": ["error", "never"],
-        "indent": ["error", 2],
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-        "prettier/prettier": "error"
-    },
-    "parserOptions": {
-        "ecmaVersion": 6,
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true
-        }
+    'prettier/prettier': 'error',
+    'import/prefer-default-export': 'off'
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
     }
+  }
 };
